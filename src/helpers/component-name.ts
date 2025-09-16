25dfbf9802d0281df29efb9bc4899d0a4ad0f4ca
Артем Анпилов <d19amamac@d19amaMac.local>
2025-09-16 12:30:29 +0300

    feat: COM-39 Обновлен AppTextarea, helpers/component-name

--- src/helpers/component-name.ts
+++ src/helpers/component-name.ts
@@ -1,3 +1,25 @@
+import type { ComponentInternalInstance } from 'vue';
+
+function randomNumber(): number {
+  return Math.round(Math.random() * 1000000);
+}
+
 export function componentName(prefix: string = 'component'): string {
-  return `${prefix}-${Math.random() * 1000}`.toLowerCase();
+  return `${prefix}-${randomNumber()}`.toLowerCase();
+}
+
+export function componentNameByInstance(instance: ComponentInternalInstance | null): string {
+  const name: string | undefined = instance?.type.__name;
+
+  if (!name) {
+    return componentName();
+  }
+
+  const matched: RegExpMatchArray | null = name.match(/[A-Z][a-z]+/g);
+
+  if (!matched) {
+    return componentName(name);
+  }
+
+  return `${matched.join('-')}-${randomNumber()}`.toLowerCase();
 }
