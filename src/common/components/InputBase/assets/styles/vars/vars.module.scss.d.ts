d638442c04c958a680737340883535d8d5f0873c
Артем Анпилов <d19amamac@d19amaMac.local>
2025-09-19 16:59:34 +0300

    feat: COM-38 Доработка AppInputFile, AppDatePicker

--- src/common/components/InputBase/assets/styles/vars/vars.module.scss.d.ts
+++ src/common/components/InputBase/assets/styles/vars/vars.module.scss.d.ts
@@ -1,11 +1,14 @@
 export interface Styles {
   'common-base-input-wrapper-bg': string;
+  'common-base-input-wrapper-border-color': string;
   'common-base-input-wrapper-border': string;
   'common-base-input-wrapper-border-radius': string;
   'common-base-input-label-color': string;
   'common-base-input-label-required-color': string;
   'common-base-input-hint-color': string;
   'common-base-input-error-color': string;
+  'common-base-input-button-bg': string;
+  'common-base-input-button-color': string;
 }
 
 export type ClassNames = keyof Styles;
