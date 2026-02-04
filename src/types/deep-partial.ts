39ed3a6d6af680f5856475ab6d1c5506e6331a28
Артем Анпилов <d19amamac@d19amaMac.local>
2026-02-04 14:53:55 +0300

    feat: COM-50 updates

---
+++ src/types/deep-partial.ts
@@ -0,0 +1,8 @@
+/**
+ * Утилита для изменения свойств объекта на optional с любой вложенностью
+ */
+export type DeepPartial<T> = {
+  [K in keyof T]: T[K] extends Record<string, unknown>
+    ? DeepPartial<T[K]>
+    : never;
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export type { GenericItem } from './generic-item';
 export type { SizeDimensions } from './size-dimensions';
 export type { HTMLElementClass } from './html-element-class';
+export type { DeepPartial } from './deep-partial';
 export type {
   GlobalPropTheme,
   GlobalPropSize,
