99386313ccf705f8b2a35492c5033e7bbf49041f
Артем Анпилов <d19amamac@d19amaMac.local>
2026-05-14 12:23:07 +0300

    Добавлен новый компонент CommonLineClamp, v2.5.0

---
+++ src/modules/CommonLineClamp/CommonLineClamp.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { CommonLineClampProps } from './types';
+
+withDefaults(defineProps<CommonLineClampProps>(), {
+  lines: 1,
+});
+</script>
+
+<template>
+  <span class="common-line-clamp">
+    <slot />
+  </span>
+</template>
+
+<style lang="scss">
+.common-line-clamp {
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-line-clamp: v-bind(lines);
+  -webkit-box-orient: vertical;
+  overflow-wrap: anywhere;
+}
+</style>
